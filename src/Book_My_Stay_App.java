3b168b5d5876baa396aa75ad4e5edfcdb3788863
abhaysingh2409 <singhpratapabhay2006@gmail.com>
2026-03-16 14:09:09 +0530

    first commit

---
+++ src/Book_My_Stay_App.java
@@ -0,0 +1,8 @@
+public class Book_My_Stay_App {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully");
+    }
+
+}
